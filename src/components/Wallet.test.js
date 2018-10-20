99c3c929d50f404c4d961501ef7b02761ddf20f0
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 22:53:56 +0530

    added test for props render

--- src/components/Wallet.test.js
+++ src/components/Wallet.test.js
@@ -3,9 +3,14 @@ import { shallow } from 'enzyme'
 import { Wallet } from './Wallet'
 
 describe('Wallet', () => {
-  const wallet = shallow(<Wallet />)
+  const props = { balance: 0 }
+  const wallet = shallow(<Wallet {...props} />)
 
   it('renders component successfully', () => {
     expect(wallet).toMatchSnapshot()
+  })
+
+  it('displays balance from props', () => {
+    expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 0')
   })
 })
