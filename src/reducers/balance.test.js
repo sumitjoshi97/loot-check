7a38a9eea2604edf786d68ce0b59ec19a4277c35
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 23:32:51 +0530

    added tests for adding and withdrawing amount

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -3,12 +3,47 @@ import balanceReducer from './balance'
 import * as actionTypes from '../actions/actionTypes'
 
 describe('balance Reducer', () => {
+  const balance = 5
   it('sets a balance', () => {
-    const balance = 10
     const balanceData = {
       type: actionTypes.SET_BALANCE,
       balance
     }
-    expect(balanceReducer(undefined, balanceData)).toEqual({balance})
+    expect(balanceReducer(undefined, balanceData)).toEqual({ balance })
+  })
+
+  it('deposits into balance', () => {
+    const initialState = {
+      balance
+    }
+    const deposit = 10
+    const depositData = {
+      type: actionTypes.DEPOSIT,
+      deposit
+    }
+
+    const expectedState = {
+      balance: initialState.balance + deposit
+    }
+
+    expect(balanceReducer(initialState, depositData)).toEqual(expectedState)
+  })
+
+  it('withdraws from balance', () => {
+    const initialState = {
+      balance
+    }
+
+    const withdraw = 5
+    const actionData = {
+      type: actionTypes.WITHDRAW,
+      withdraw
+    }
+
+    const expectedState = {
+      balance: initialState.balance - withdraw
+    }
+
+    expect(balanceReducer(initialState, actionData)).toEqual(expectedState)
   })
 })
