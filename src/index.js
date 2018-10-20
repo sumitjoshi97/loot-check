92dc0994f00a25afad4624f56dd5be21cec6c3e5
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 23:34:30 +0530

    redux dev-tools added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import { createStore } from 'redux'
+import { createStore} from 'redux'
 import rootReducer from './reducers/balance'
 import { Provider } from 'react-redux'
-import App from './App'
+import App from './components/App'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const app = (
   <Provider store={store}>
