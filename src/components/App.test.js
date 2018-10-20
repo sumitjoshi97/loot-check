12321b1278746a0e8c9e4d546a65762c81b6f079
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 22:54:29 +0530

    test to check wallet component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,9 +4,13 @@ import { shallow } from 'enzyme'
 import App from './App'
 
 describe('App', () => {
-  const app = shallow(<App/>)
+  const app = shallow(<App />)
 
   it('renders successfully', () => {
     expect(app).toMatchSnapshot()
+  })
+
+  it('contains a wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true)
   })
 })
