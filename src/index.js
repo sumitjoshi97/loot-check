4331383b51e95d8f6f10a8c38d2a0d998dcaadba
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 22:40:00 +0530

    root render component

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import { createStore } from 'redux'
+import rootReducer from './reducers/balance'
+import { Provider } from 'react-redux'
+import App from './App'
+
+const store = createStore(rootReducer)
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+render(app, document.getElementById('root'))
