7cd1f9380e44494f13e4ca0f447fd730d1dbabe6
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 23:34:18 +0530

    actions for deposit and withdrawl added

--- src/actions/balance.js
+++ src/actions/balance.js
@@ -4,3 +4,13 @@ export const setBalance = balance => ({
   type: actionTypes.SET_BALANCE,
   balance
 })
+
+export const deposit = deposit => ({
+  type: actionTypes.DEPOSIT,
+  deposit
+})
+
+export const withdraw = withdraw => ({
+  type: actionTypes.WITHDRAW,
+  withdraw
+})
