83e929a5757622cbd7cc4d80e8e87d372450e9fb
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-20 23:33:46 +0530

    tests for deposit and withdrawl actions added

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -2,10 +2,24 @@ import * as actionTypes from './actionTypes'
 import * as actions from './balance'
 
 describe('Balance Action', () => {
-  it('creates action to set balance', () => {
+  it('sets balance', () => {
     const balance = 0
     const expectedAction = { type: actionTypes.SET_BALANCE, balance }
 
     expect(actions.setBalance(balance)).toEqual(expectedAction)
+  })
+
+  it('deposits into balance', () => {
+    const deposit = 10
+    const expectedAction = {type: actionTypes.DEPOSIT, deposit}
+
+    expect(actions.deposit(deposit)).toEqual(expectedAction)
+  })
+
+  it('withdraws amount from balance', () => {
+    const withdraw = 10
+    const expectedAction = {type: actionTypes.WITHDRAW, withdraw}
+
+    expect(actions.withdraw(withdraw)).toEqual(expectedAction)
   })
 })
