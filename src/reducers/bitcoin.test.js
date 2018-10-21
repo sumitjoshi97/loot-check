9bb5252298c68a2ae9b242e1415497e7f17090ce
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-21 23:20:24 +0530

    tests for bitcoin reducer added

---
+++ src/reducers/bitcoin.test.js
@@ -0,0 +1,19 @@
+import bitcoinReducer from './bitcoin'
+import { FETCH_BITCOIN } from '../actions/actionTypes'
+
+describe('bitcoin Reducer', () => {
+  const bitcoinData = { bpi: 'bitcoin price index' }
+
+  const initialState = {
+    bitcoin: null
+  }
+
+  it('fetches and sets the bitcoin data', () => {
+    expect(
+      bitcoinReducer(initialState, {
+        type: FETCH_BITCOIN,
+        bitcoin: bitcoinData
+      })
+    ).toEqual({ bitcoin: bitcoinData })
+  })
+})
