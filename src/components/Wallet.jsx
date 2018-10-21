0c56df1fdc2040845f73f0b02e9ebc8d2b48c51a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-21 15:48:17 +0530

    buttons added for deposit and withdrawl

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { deposit, withdraw } from '../actions/balance'
 
 export class Wallet extends Component {
+  state = {
+    balance: 0
+  }
+
+  updateBalance = e => {
+    this.setState({
+      balance: parseInt(e.target.value, 10)
+    })
+  }
   render() {
     return (
       <div>
         <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
+        <input className="input-wallet" onChange={this.updateBalance} />
+        <button
+          className="btn-deposit"
+          onClick={() => this.props.deposit(this.state.balance)}
+        >
+          Deposit
+        </button>
+        <button
+          className="btn-withdraw"
+          onClick={() => this.props.withdraw(this.state.balance)}
+        >
+          Withdrawl
+        </button>
       </div>
     )
   }
@@ -15,4 +38,7 @@ const mapStateToProps = state => ({
   balance: state.balance
 })
 
-export default connect(mapStateToProps)(Wallet)
+export default connect(
+  mapStateToProps,
+  { deposit, withdraw }
+)(Wallet)
