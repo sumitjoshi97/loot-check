20c20553372a9f10faa67f593999eb850cde12ba
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-21 22:30:13 +0530

    tests added for cookie reinitalization

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,23 +1,33 @@
 import balanceReducer from './balance'
-
+import balanceReducer2 from './balance'
 import * as actionTypes from '../actions/actionTypes'
 
 describe('balance Reducer', () => {
-  const balance = 5
-  it('sets a balance', () => {
-    const balanceData = {
-      type: actionTypes.SET_BALANCE,
-      balance
-    }
-    expect(balanceReducer(undefined, balanceData)).toEqual({ balance })
+  const initialState = {
+    balance: 5
+  }
+
+  describe('when initializing', () => {
+    it('sets a balance', () => {
+      const balanceAction = {
+        type: actionTypes.SET_BALANCE,
+        balance: 10
+      }
+      expect(balanceReducer(initialState, balanceAction)).toEqual({
+        balance: 10
+      })
+    })
+
+    describe('then reinitializes', () => {
+      it('reads balance from cookies', () => {
+        expect(balanceReducer2(initialState, {})).toEqual({ balance: 10 })
+      })
+    })
   })
 
   it('deposits into balance', () => {
-    const initialState = {
-      balance
-    }
     const deposit = 10
-    const depositData = {
+    const depositAction = {
       type: actionTypes.DEPOSIT,
       deposit
     }
@@ -26,14 +36,10 @@ describe('balance Reducer', () => {
       balance: initialState.balance + deposit
     }
 
-    expect(balanceReducer(initialState, depositData)).toEqual(expectedState)
+    expect(balanceReducer(initialState, depositAction)).toEqual(expectedState)
   })
 
   it('withdraws from balance', () => {
-    const initialState = {
-      balance
-    }
-
     const withdraw = 5
     const actionData = {
       type: actionTypes.WITHDRAW,
