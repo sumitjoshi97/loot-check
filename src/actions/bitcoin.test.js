9a7d35505e7f014ab1818049fc7703bc58f084fe
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-22 12:52:09 +0530

    replaced fetch mock to axios mock

--- src/actions/bitcoin.test.js
+++ src/actions/bitcoin.test.js
@@ -3,22 +3,30 @@ import { fetchBitcoin } from './bitcoin'
 
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import fetchMock from 'fetch-mock'
-
-const createMockStore = configureMockStore([thunk])
-const store = createMockStore({ bitcoin: {} })
-
-const mockResponse = { body: { bpi: 'bitcoin price index' } }
-
-fetchMock.get('https://api.coindesk.com/v1/bpi/currentprice.json', mockResponse)
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 
 describe('bitcoin Actions', () => {
+  let store
+  let axiosMock
+  const mockResponse = { body: { bpi: 'bitcoin price index' } }
+
+  beforeEach(() => {
+    axiosMock = new MockAdapter(axios)
+    const createMockStore = configureMockStore([thunk])
+    store = createMockStore({ bitcoin: {} })
+  })
+
   it('creates an async action to fetch bitcoin value', () => {
+    axiosMock
+      .onGet('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .reply(200, mockResponse.body)
+
     const expectedActions = [
       { type: FETCH_BITCOIN, bitcoin: mockResponse.body }
     ]
 
-    store.dispatch(fetchBitcoin()).then(() => {
+    fetchBitcoin()(store.dispatch).then(() => {
       expect(store.getActions()).toEqual(expectedActions)
     })
   })
