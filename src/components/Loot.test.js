ee3a271746d5703d933e8db22e4c5c4878db60ec
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-22 12:50:57 +0530

    tests added for fn called in component did mount

--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import { Loot } from './Loot'
 
 describe('Loot Component', () => {
-  const loot = shallow(<Loot />)
+  const mockFetchBitcoin = jest.fn()
+  const props = { balance: 10, bitcoins: {}}
+  let loot = shallow(<Loot {...props} />)
 
   it('renders properly', () => {
     expect(loot).toMatchSnapshot()
+  })
+
+  describe('when mounted', () => {
+    beforeEach(() => {
+      props.onFetchBitcoin = mockFetchBitcoin
+      loot = mount(<Loot {...props} />)
+    })
+
+    it('dispatches `fetchBitcoin()` it received from props', () => {
+      expect(mockFetchBitcoin).toHaveBeenCalled()
+    })
   })
 })
