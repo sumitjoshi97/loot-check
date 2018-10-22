46a84c1c0e3b62982fbdaad880844c6d440ef6ee
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-10-22 12:52:33 +0530

    replaced fetch api by axios

--- src/actions/bitcoin.js
+++ src/actions/bitcoin.js
@@ -1,12 +1,13 @@
+import axios from 'axios'
 import { FETCH_BITCOIN } from './actionTypes'
 
 export const fetchBitcoin = () => dispatch => {
-  return fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(res => res.json())
-    .then(data =>
-      dispatch({
-        type: FETCH_BITCOIN,
-        bitcoin: data
-      })
-    )
+  return axios
+    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(res => {
+    dispatch({
+      type: FETCH_BITCOIN,
+      bitcoin: res.data
+    })
+  })
 }
